6c9f55ed28bb528937e74f3f58d7ede0a1f5e0d6
arthurAlcebiades <arthur.alcebiades@hotmail.com>
2022-04-11 11:51:06 -0300

    commit de uma página de cadastro de produtos

---
+++ CursoMVC/Models/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=localhost\SQLEXPRESS;Database=SqlMVC;Trusted_Connection=True");
+        }
+
+    }
+}
